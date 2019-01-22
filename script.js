f678c7aa1746bb923ec3345e187ae144ac7b8db1
Linda Wallace <linda3nt@gmail.com>
2019-01-22 15:19:37 -0700

    Apply Assignment 5

---
+++ script.js
@@ -0,0 +1,23 @@
+console.log("hello");
+
+var submitButton = document.getElementById("lindaSubmit");
+
+submitButton.addEventListener("click", function(event) {
+    event.preventDefault();
+    var submitNow = confirm("Are you sure you want to submit this form?");
+    if (submitNow === true) {
+        console.log("submit is true");
+    }else {
+        alert("Have a nice day.  Maybe you will submit another day.");
+}});
+
+var resetButton = document.getElementById("lindaReset");
+
+resetButton.addEventListener("click", function(event) {
+    event.preventDefault();
+    var submitNow = confirm("Are you sure you want to reset?");
+    if (submitNow === true) {
+        console.log("reset is true");
+    }else {
+        alert("Reset is cancelled");
+}});
